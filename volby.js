8a2670bf06acf4de69194513a9c1f0a55b4a64ba
Tomas Kafka <tk@tomaskafka.com>
2012-11-01 01:49:43 +0100

    vysledky voleb podle Janecka - 2 kandidati z kazdeho kraje podle preferencncich hlasu

--- volby.js
+++ volby.js
@@ -1,57 +1,151 @@
 "use strict";
 
 var request = require('request'),
-	// $ = require('jquery');
+// $ = require('jquery');
+	async = require('async'),
 	cheerio = require('cheerio'),
 	encoding = require("encoding");
 
-var volbyTableUrl = 'http://www.volby.cz/pls/ps2010/ps11?xjazyk=CZ&xv=1&xt=1';
+var volbyBaseUrl = 'http://www.volby.cz/pls/ps2010/ps111?xjazyk=CZ&xstrana=0&xv=1&xt=1&xkraj=';
 
-getUrl(volbyTableUrl, 'ISO-8859-2', function(err, body) {
-	var $body = cheerio.load(body);
 
-	var $table = $body('table'),
-		$rows = $table.find('tr'),
-		$firstRow = $rows.eq(0),
-		$kraje = $firstRow.find('th'),
-		$secondRow = $rows.eq(2),
-		$vysledky = $secondRow.find('td a')
-		;
+var ciselnikKraju = {
+	1: 'Hlavní město Praha',
+	2: 'Středočeský kraj',
+	3: 'Jihočeský kraj',
+	4: 'Plzeňský kraj',
+	5: 'Karlovarský kraj',
+	6: 'Ústecký kraj',
+	7: 'Liberecký kraj',
+	8: 'Královéhradecký kraj',
+	9: 'Pardubický kraj',
+	10: 'Vysočina',
+	11: 'Jihomoravský kraj',
+	12: 'Olomoucký kraj',
+	13: 'Zlínský kraj',
+	14: 'Moravskoslezský kraj'
+};
 
-	var kraje = [],
-		vysledky = [];
+var kraje = [],
+	nactenychKraju = 0;
 
-	var cnt = 0;
-	$kraje.each(function(i, el) {
-		if (i < 2) { return; }
-		var name = cheerio(this).text();
-		kraje[cnt++] = name;
+for (var i = 1; i <= 14; i++) {
+	(function () { // protect closure
 
-		console.log(name);
+		var krajUrl = volbyBaseUrl + i,
+			cisloKraje = i; // closure local copy
+
+		// console.log('cislo kraje: ' + cisloKraje);
+
+		getUrl(krajUrl, 'ISO-8859-2', function (err, body) {
+			var $body = cheerio.load(body),
+				$table = $body('table'),
+				$rows = $table.find('tr');
+
+			var kandidati = [];
+
+			$rows.each(function (i) {
+				if (i < 2) {
+					return;
+				}
+
+				var $row = cheerio(this),
+					$tds = $row.find('td');
+
+				var strana = $tds.eq(1).text(),
+					jmeno = $tds.eq(3).text(),
+					vek = $tds.eq(4).text(),
+					prislusnost = $tds.eq(6).text(),
+					hlasyStr = $tds.eq(7).text().replace(/[^0-9]+/ig, ''),
+					hlasy = parseInt(hlasyStr, 10);
+
+				// console.log(hlasyStr + ' - ' + hlasy);
+
+				kandidati.push({
+					cisloKraje: cisloKraje,
+					kraj: ciselnikKraju[cisloKraje],
+					strana: strana,
+					jmeno: jmeno,
+					vek: vek,
+					prislusnost: prislusnost,
+					hlasy: hlasy
+				});
+			});
+
+			kraje[cisloKraje] = kandidati;
+
+			nactenychKraju++;
+			// console.log('kraj ' + cisloKraje + ' - ' + krajUrl);
+
+			// dirty: posledni kraj
+			if (nactenychKraju == 14) {
+				// console.log(kraje);
+				// console.log('done');
+
+				var vysledky = spocitejVysledky(kraje);
+
+				vysledkyCsv(vysledky);
+
+				// console.log(vysledky);
+			}
+		});
+	})();
+}
+
+// kraje[cisloKraje][x] = { kandidat }
+function spocitejVysledky(kraje) {
+	var vysledky = [];
+
+	kraje.map(function(kandidati, cisloKraje) {
+		kandidati.sort(function(a, b) {
+			return b.hlasy - a.hlasy;
+		});
+
+		kandidati[0].poradi = 1;
+		kandidati[1].poradi = 2;
+
+		vysledky.push(kandidati[0]);
+		vysledky.push(kandidati[1]);
 	});
 
-	cnt = 0;
-	$vysledky.each(function(i, el) {
-		if (i < 1) { return; }
-		var link = cheerio(this).attr('href');
-		vysledky[cnt++] = link;
+	return vysledky;
+}
 
-		console.log(link);
+function vysledkyCsv(vysledky) {
+	console.log(
+		'cisloKraje\t' +
+		'kraj\t' +
+		'strana\t' +
+		'jmeno\t' +
+		'vek\t' +
+		'prislusnost\t' +
+		'hlasy\t' +
+		'poradi\t'
+	)
+	vysledky.map(function(kandidat) {
+		console.log(
+			kandidat.cisloKraje + '\t' +
+			kandidat.kraj + '\t' +
+			kandidat.strana + '\t' +
+			kandidat.jmeno + '\t' +
+			kandidat.vek + '\t' +
+			kandidat.prislusnost + '\t' +
+			kandidat.hlasy + '\t' +
+			kandidat.poradi + '\t'
+		)
 	});
-
-	// console.log($body.html());
-	// console.log($table.html());
-});
-
-
+}
 
 function getUrl(url, charset, callback) {
-	request(url, function (error, response, body) {
+	request({
+		url: url,
+		encoding: 'binary'
+	}, function (error, response, body) {
 		if (!error && response.statusCode === 200) {
 			var resultBuffer = encoding.convert(body, 'UTF-8', charset);
 			var bodyUtf = resultBuffer.toString();
 
-				callback(null, bodyUtf);
+			callback(null, bodyUtf);
 		} else {
 			callback(error);
 		}
